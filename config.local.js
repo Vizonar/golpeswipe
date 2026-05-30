38b7a25677361bd02c89906b311a0d7a3e2007ac
Vizonar <113144065+Vizonar@users.noreply.github.com>
2026-05-30 14:31:45 -0300

    Create config.local.js

---
+++ config.local.js
@@ -0,0 +1,4 @@
+window.GOLPESWIPE_CONFIG = {
+  SUPABASE_URL: 'https://yfsgfzibjnqaayvcdrjz.supabase.co',
+  SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inlmc2dmemliam5xYWF5dmNkcmp6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODAwMDY3MDMsImV4cCI6MjA5NTU4MjcwM30.2BGZLNNCurSdcmOOhmcrTFUjIoULJ9RO33raCcCrlnE'
+};
